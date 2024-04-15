fa61cd6ac236614708b7a8774b3daf860c603435
mojavemf <funnyfailiers@gmail.com>
2024-04-15 18:33:00 +0100

    Clean get_latest_release func

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,44 +1,53 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use colored::{control, ColoredString, Colorize};
+use lazy_static::lazy_static;
+use rdev::{listen, Event, EventType};
+use reqwest::Client;
+use serde::Serialize;
 use serde_json::Value;
-use tauri::{command, generate_context, generate_handler, Builder, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, Window, WindowEvent};
-use std::{thread::{self, sleep}, time::Duration};
-use std::sync::{Arc, Mutex};
 use std::ffi::OsString;
 use std::os::windows::ffi::OsStrExt;
-use rdev::{listen, Event, EventType};
-use lazy_static::lazy_static;
-use reqwest::Client;
-use colored::{Colorize, ColoredString, control};
-use win_msgbox::{w, YesNo};
-use serde::Serialize;
+use std::sync::{Arc, Mutex};
+use std::{
+    thread::{self, sleep},
+    time::Duration,
+};
 use sysinfo::System;
-use tokio::{fs::{self, File}, io::AsyncReadExt};
+use tauri::{
+    command, generate_context, generate_handler, Builder, CustomMenuItem, Manager, SystemTray,
+    SystemTrayEvent, SystemTrayMenu, Window, WindowEvent,
+};
+use tokio::{
+    fs::{self, File},
+    io::AsyncReadExt,
+};
+use win_msgbox::{w, YesNo};
 
 #[derive(Clone, Serialize)]
 struct Payload {
-  message: String,
+    message: String,
 }
 
 #[derive(Clone, Serialize)]
 struct PayloadUpdate {
-  message: bool,
+    message: bool,
 }
 
 #[derive(Clone, Serialize)]
 struct Payload2 {
-  args: Vec<String>,
-  cwd: String,
+    args: Vec<String>,
+    cwd: String,
 }
 
 struct Server {
-    window: Window
+    window: Window,
 }
 
 impl ws::Handler for Server {
     fn on_message(&mut self, data: ws::Message) -> ws::Result<()> {
         let mut connected = CONNECTED.lock().unwrap();
-        
+
         if *connected {
             return Ok(());
         }
@@ -47,11 +56,13 @@ impl ws::Handler for Server {
         let mut parts = data_string.split(",");
         let type_value = match parts.next() {
             Some(val) => val.trim(),
-            None => return Ok(())
+            None => return Ok(()),
         };
 
         if type_value == "connect" {
-            self.window.emit("update", PayloadUpdate { message: true }).unwrap();
+            self.window
+                .emit("update", PayloadUpdate { message: true })
+                .unwrap();
             *connected = true;
         }
 
@@ -62,7 +73,9 @@ impl ws::Handler for Server {
         let mut connected = CONNECTED.lock().unwrap();
 
         if *connected {
-            self.window.emit("update", PayloadUpdate { message: false }).unwrap();
+            self.window
+                .emit("update", PayloadUpdate { message: false })
+                .unwrap();
             *connected = false;
         }
     }
@@ -70,62 +83,51 @@ impl ws::Handler for Server {
 
 #[command]
 fn kill_roblox() -> bool {
-    return match System::new_all().processes_by_name("RobloxPlayerBeta.exe").next() {
+    return match System::new_all()
+        .processes_by_name("RobloxPlayerBeta.exe")
+        .next()
+    {
         Some(process) => process.kill(),
-        _ => false
+        _ => false,
     };
 }
 
 #[command]
 fn is_roblox_running() -> bool {
-    return System::new_all().processes_by_name("RobloxPlayerBeta.exe").next().is_some();
+    return System::new_all()
+        .processes_by_name("RobloxPlayerBeta.exe")
+        .next()
+        .is_some();
 }
 
 async fn get_latest_release() -> Option<(String, String)> {
     let client = Client::new();
-    let response = client.get("https://api.github.com/repos/BitdancerStudios/KrampUI/releases/latest")
+    let response = client
+        .get("https://api.github.com/repos/BitdancerStudios/KrampUI/releases/latest")
         .header("User-Agent", "KrampUI")
         .timeout(Duration::from_secs(5))
         .send()
-        .await;
+        .await
+        .ok()?;
 
-    return match response {
-        Ok(response) =>  match response.status().is_success() {
-            true => {
-                let json: Value = match response.json().await {
-                    Ok(json) => json,
-                    Err(_) => return None
-                };
+    // Converted match into Option try's Some(value)? => value None? => returns none
 
-                let version = match json.get("tag_name") {
-                    Some(version) => match version.as_str() {
-                        Some(version) => version.replace("v", ""),
-                        None => return None
-                    },
-                    None => return None
-                };
+    if !response.status().is_success() {
+        return None;
+    }
 
-                let release = match json.get("html_url") {
-                    Some(release) => match release.as_str() {
-                        Some(release) => release.to_string(),
-                        None => return None
-                    },
-                    None => return None
-                };
+    let json: Value = response.json().await.ok()?;
+    let version = json.get("tag_name")?.as_str()?.replace("v", "");
+    let release = json.get("html_url")?.as_str()?.to_string();
 
-                return Some((version, release));
-            },
-            false => None
-        },
-        Err(_) => None
-    };
+    return Some((version, release));
 }
 
 #[command]
 async fn create_directory(path: String) -> bool {
     return match fs::create_dir_all(&path).await {
         Ok(_) => true,
-        Err(_) => false
+        Err(_) => false,
     };
 }
 
@@ -133,7 +135,7 @@ async fn create_directory(path: String) -> bool {
 async fn write_file(path: String, data: String) -> bool {
     return match fs::write(&path, &data).await {
         Ok(_) => true,
-        Err(_) => false
+        Err(_) => false,
     };
 }
 
@@ -141,7 +143,7 @@ async fn write_file(path: String, data: String) -> bool {
 async fn delete_directory(path: String) -> bool {
     return match fs::remove_dir_all(&path).await {
         Ok(_) => true,
-        Err(_) => false
+        Err(_) => false,
     };
 }
 
@@ -149,7 +151,7 @@ async fn delete_directory(path: String) -> bool {
 async fn delete_file(path: String) -> bool {
     return match fs::remove_file(&path).await {
         Ok(_) => true,
-        Err(_) => false
+        Err(_) => false,
     };
 }
 
@@ -166,11 +168,18 @@ fn init_key_events(window: Window) {
 
     if !*key_events_initialized {
         *key_events_initialized = true;
-        
+
         thread::spawn(move || {
-            let callback = move | event: Event | {
+            let callback = move |event: Event| {
                 if let EventType::KeyRelease(key) = event.event_type {
-                    window.emit("key-press", Payload { message: format!("{:?}", key) }).unwrap();
+                    window
+                        .emit(
+                            "key-press",
+                            Payload {
+                                message: format!("{:?}", key),
+                            },
+                        )
+                        .unwrap();
                 }
             };
 
@@ -186,30 +195,29 @@ fn init_websocket(window: Window, port: u16) {
     if !*websocket_initialized {
         *websocket_initialized = true;
 
-        
-
         thread::spawn(move || {
-            ws::listen(format!("127.0.0.1:{}", port), move | out: ws::Sender | { 
+            ws::listen(format!("127.0.0.1:{}", port), move |out: ws::Sender| {
                 let cloned_window = window.clone();
                 *WEBSOCKET.lock().unwrap() = Some(out.clone());
-                return Server { window: cloned_window };
-            }).ok();
+                return Server {
+                    window: cloned_window,
+                };
+            })
+            .ok();
         });
 
-        thread::spawn(move || {
-            loop {
-                if let Some(websocket) = WEBSOCKET.lock().unwrap().clone() {
-                    websocket.send("kr-ping").unwrap();
-                }
+        thread::spawn(move || loop {
+            if let Some(websocket) = WEBSOCKET.lock().unwrap().clone() {
+                websocket.send("kr-ping").unwrap();
+            }
 
-                sleep(Duration::from_millis(250));
-            };
+            sleep(Duration::from_millis(250));
         });
     }
 }
 
 #[command]
-fn execute_script(text: &str) {    
+fn execute_script(text: &str) {
     if let Some(websocket) = WEBSOCKET.lock().unwrap().clone() {
         websocket.send(text).unwrap();
     }
@@ -223,9 +231,9 @@ fn log(message: String, _type: Option<String>) {
             "success" => Some("[  OK  ]".green()),
             "warn" => Some("[ WARN ]".yellow()),
             "error" => Some("[ FAIL ]".red()),
-            _ => None
+            _ => None,
         },
-        None => None
+        None => None,
     };
 
     if let Some(prefix) = prefix {
@@ -239,14 +247,14 @@ fn log(message: String, _type: Option<String>) {
 async fn validate_executable(executable_path: String) -> (bool, String) {
     let mut file = match File::open(executable_path).await {
         Ok(file) => file,
-        Err(_) => return (false, "Failed to open file!".to_string())
+        Err(_) => return (false, "Failed to open file!".to_string()),
     };
 
     let mut buffer = Vec::new();
 
     match file.read_to_end(&mut buffer).await {
-        Ok(_) => {},
-        Err(_) => return (false, "Failed to read executable".to_string())
+        Ok(_) => {}
+        Err(_) => return (false, "Failed to read executable".to_string()),
     };
 
     let min_length = 4;
@@ -285,22 +293,31 @@ async fn main() {
 
         let latest_version_number = match latest_version.replace(".", "").parse::<i32>() {
             Ok(number) => Some(number),
-            Err(_) => None
+            Err(_) => None,
         };
 
         let current_version_number = match current_version.replace(".", "").parse::<i32>() {
             Ok(number) => Some(number),
-            Err(_) => None
+            Err(_) => None,
         };
 
-        if latest_version_number.is_some() && current_version_number.is_some() && latest_version_number.unwrap() > current_version_number.unwrap() {
-            let message = format!("Would you like to update?\nYou are on v{}, the latest is v{}.", current_version, latest_version);
-            let wide_message: Vec<u16> = OsString::from(&message).encode_wide().chain(Some(0)).collect();
+        if latest_version_number.is_some()
+            && current_version_number.is_some()
+            && latest_version_number.unwrap() > current_version_number.unwrap()
+        {
+            let message = format!(
+                "Would you like to update?\nYou are on v{}, the latest is v{}.",
+                current_version, latest_version
+            );
+            let wide_message: Vec<u16> = OsString::from(&message)
+                .encode_wide()
+                .chain(Some(0))
+                .collect();
             let response = win_msgbox::information::<YesNo>(wide_message.as_ptr())
                 .title(w!("KrampUI"))
                 .show()
                 .unwrap();
-            
+
             if response == YesNo::Yes {
                 open::that(link).unwrap();
                 return;
@@ -311,7 +328,7 @@ async fn main() {
     let toggle = CustomMenuItem::new("toggle".to_string(), "Toggle");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let tray = SystemTrayMenu::new().add_item(toggle).add_item(quit);
-    
+
     Builder::default()
         .on_window_event(|e| {
             if let WindowEvent::Resized(_) = e.event() {
@@ -319,20 +336,35 @@ async fn main() {
             }
         })
         .system_tray(SystemTray::new().with_menu(tray))
-        .on_system_tray_event(| app, e | match e {
+        .on_system_tray_event(|app, e| match e {
             SystemTrayEvent::MenuItemClick { id, .. } => {
                 let window = app.get_window("main").unwrap();
-                
+
                 match id.as_str() {
-                    "toggle" => window.emit("toggle", Payload { message: "".to_string() }).unwrap(),
-                    "quit" => window.emit("exit", Payload { message: "".to_string() }).unwrap(),
+                    "toggle" => window
+                        .emit(
+                            "toggle",
+                            Payload {
+                                message: "".to_string(),
+                            },
+                        )
+                        .unwrap(),
+                    "quit" => window
+                        .emit(
+                            "exit",
+                            Payload {
+                                message: "".to_string(),
+                            },
+                        )
+                        .unwrap(),
                     _ => {}
                 }
             }
             _ => {}
-          })
+        })
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            app.emit_all("single-instance", Payload2 { args: argv, cwd }).unwrap();
+            app.emit_all("single-instance", Payload2 { args: argv, cwd })
+                .unwrap();
         }))
         .invoke_handler(generate_handler![
             init_websocket,
