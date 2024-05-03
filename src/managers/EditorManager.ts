d1870b8582a6fbb41caa94c9afe6b00647a8d516
Pixeluted <pixeluted@proton.me>
2024-05-03 13:40:25 +0200

    Fixed support for comments

--- src/managers/EditorManager.ts
+++ src/managers/EditorManager.ts
@@ -531,6 +531,8 @@ export default class EditorManager {
             ],
             [/"(?:\\.|[^\\"])*"|'(?:\\.|[^\\'])*'/gm, "string"],
             [/-?\b\d+(\.\d+)?(e[+-]?\d+)?\b/gi, "number"],
+            [/--\[\[.*?\]\]/s, "comment"],
+            [/--.*$/, "comment"],
           ],
         },
       });
